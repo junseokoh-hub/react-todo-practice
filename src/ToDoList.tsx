6e8d7110c3463f744621a5759da5164dc6a2d917
junseokoh-hub <lunulatecat@naver.com>
2022-07-05 22:31:20 +0900

    form validation with handleSubmit fn & formState object

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -35,20 +35,46 @@ import { useForm } from "react-hook-form";
 function ToDoList() {
   // console.log(useForm());
   // onChange is handled with register function in useForm
-  const { register, watch } = useForm();
+  const { register, handleSubmit, formState } = useForm();
   // register : returns object containing onChange event
   // spread object of register fn as props of input
   // watch : literally watch inputs
-  console.log(watch());
+  // formstate.errors: error handling
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
+
   return (
     <div>
-      <form>
-        <input {...register("email")} placeholder="Email" />
-        <input {...register("firstName")} placeholder="First Name" />
-        <input {...register("lastName")} placeholder="Last Name" />
-        <input {...register("username")} placeholder="Username" />
-        <input {...register("password")} placeholder="Password" />
-        <input {...register("password1")} placeholder="Password1" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        <input {...register("email", { required: true })} placeholder="Email" />
+        <input
+          {...register("firstName", { required: true })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("lastName", { required: true })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("password", { required: true, minLength: 5 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("password1", {
+            required: "Password is required",
+            minLength: { value: 5, message: "Your password is too short" },
+          })}
+          placeholder="Password1"
+        />
         <button>Add</button>
       </form>
     </div>
